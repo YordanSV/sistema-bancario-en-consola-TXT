e478201594cd83bb4ab4bbffa843b47acb7dd7aa
Yordan Salazar <yordan10_pv@hotmail.com>
2023-05-21 18:52:58 -0600

    Falta probar las funciones deleteUser y modifyUser

--- Usuario.java
+++ Usuario.java
@@ -5,6 +5,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.File;
+
 // Programa de explicación básica en Java
 
 public class Usuario {
@@ -13,165 +16,263 @@ public class Usuario {
     public String clave;
     public int identificacion;
     public int edad;
-    public boolean administrador;
+    public boolean esAdministrador;
 
-    public Usuario(String nombre, String apellido, int identificacion, String clave, int edad, boolean administrador) {
+    public Usuario(String nombre, String apellido, int identificacion, String clave, int edad,
+            boolean esAdministrador) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.clave = clave;
         this.identificacion = identificacion;
         this.edad = edad;
-        this.administrador = administrador;
+        this.esAdministrador = esAdministrador;
     }
 
     public Usuario() {
     }
 
-    public static boolean esAdministrador(boolean administrador) { // requestUser Solicitar usuario
-        return administrador;
+    public String getClave() { // requestUser Solicitar usuario
+        return clave;
     }
 
-    public static void main(String[] args) {
-        // Código para limpiar la terminal
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-        /*
-        Scanner scanner = new Scanner(System.in);
-        int opcion = 0;
-        while (opcion != 4) {
-            opcion = scanner.nextInt();
-            scanner.nextLine(); // Consumir el salto de línea
-            switch (opcion) {
-                case 1:
-                    
-                    break;
-            }
-        }
-        */
-       Usuario usuario;
-       do{
-        usuario = singIn();
-       }while(usuario == null);
-
-        //createUser();
+    public boolean esAdministrador() { // requestUser Solicitar usuario
+        return esAdministrador;
     }
+
+    // ==================== Iniciar Sesion ====================
     public static Usuario singIn() { // Iniciar sesion
-        String nombreArchivo = "USUARIO.txt";
         Scanner scanner = new Scanner(System.in);
         Usuario usuario = null;
-        System.out.println("=== Iniciar sesion ===");
-        System.out.println("Ingrese su identificacion: ");
-        int identificacion = scanner.nextInt();
-        scanner.nextLine();
-        // Código para limpiar la terminal
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        while (true) {
+            System.out.println("=== Iniciar sesion ===");
+            System.out.println("Ingrese su identificacion: ");
+            int identificacion = scanner.nextInt();
+            scanner.nextLine();
+            usuario = getUser(identificacion);
+            if (usuario == null) {
+                System.out.print("\033[H\033[2J"); // Código para limpiar la terminal
+                System.out.flush();
+                System.out.println("El usuario no existe intente de nuevo");
+                continue;
+            }
+            System.out.println("Ingrese su clave: ");
+            String clave = scanner.nextLine();
+            if (!(usuario.getClave().equals(clave))) { // Si las cadenas son iguales
+                System.out.print("\033[H\033[2J"); // Código para limpiar la terminal
+                System.out.flush();
+                System.out.println("clave incorrecta intente de nuevo");
+                continue;
+            }
+            break;
+        }
+        return usuario;
+    }
 
+    // ==================== Obtener usuario ====================
+    public static Usuario getUser(int identificacion) { // Iniciar sesion
+        String nombreArchivo = "USUARIO.txt";
+        Usuario usuario = null;
         try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
-            int contador = 0;
-            List<String> lista = new ArrayList<>();
             while ((linea = br.readLine()) != null) {
-                lista.add(linea);
-                if(contador == 5 && Integer.parseInt(lista.get(2)) == identificacion){
-                    System.out.println("Ingrese su clave: ");
-                    String clave = scanner.nextLine();
-                    if(clave.equals(lista.get(3))){
-                        System.out.println("Se ha iniciado sesion exitosamente");
-                        usuario = new Usuario(lista.get(0), lista.get(1), Integer.parseInt(lista.get(2)), lista.get(3), Integer.parseInt(lista.get(4)), Boolean.parseBoolean(lista.get(5)));
-                        break;
-                    }else{
-                        // Código para limpiar la terminal
-                        System.out.print("\033[H\033[2J");
-                        System.out.flush();
-                        System.out.println("Clave incorrecta intente de nuevo");
-                        return null;
-                    }
-                }else if(contador == 5){
-                    lista.clear();
-                    contador = 0;
-                }else{
-                    contador = contador+1;
+                String[] atributos = linea.split(",");
+                if (identificacion == Integer.parseInt(atributos[2])) {
+                    usuario = new Usuario(atributos[0], atributos[1], Integer.parseInt((atributos[2])), atributos[3],
+                            Integer.parseInt((atributos[4])), Boolean.parseBoolean(atributos[5]));
+                    return usuario;
                 }
             }
         } catch (IOException e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
-        if(usuario == null){
-            System.out.println("El usuario no existe intente de nuevo");
-        }
-        scanner.close();
         return usuario;
     }
 
-    public static void createUser() { // crear usuario
+    // ==================== Crear usuario ====================
+    public static void createUser() {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese su nombre: ");
+        System.out.println("Ingrese el nombre: ");
         String nombre = scanner.nextLine();
-
-        System.out.println("Ingrese su apellido: ");
+        System.out.println("Ingrese el apellido: ");
         String apellido = scanner.nextLine();
-
         System.out.println("Ingrese su identificacion: ");
         int identificacion = scanner.nextInt();
         scanner.nextLine(); // Limpiar el búfer después de leer un entero
-
         System.out.println("Ingrese su clave: ");
         String clave = scanner.nextLine();
-
         System.out.println("Ingrese su edad: ");
         int edad = scanner.nextInt();
         scanner.nextLine();
-
         System.out.println("Desea que sea administrador? ");
-        System.out.println("1. Sí");
+        System.out.println("1. Si");
         System.out.println("2. No");
         int esAdministrador = scanner.nextInt();
         scanner.nextLine();
-        if(esAdministrador == 1){
-            saveUser(nombre, apellido, identificacion,clave, edad, true);
-        }else{
-            saveUser(nombre, apellido, identificacion, clave, edad, false);
-        }
-        
-
-        // Llamar al método para escribir en el archivo
-        scanner.close();
+        saveUser(nombre, apellido, identificacion, clave, edad, (esAdministrador == 1) ? true : false);
     }
 
-    public static void saveUser(String nombre, String apellido, int identificacion, String clave, int edad, boolean esAdministrador) {
+    // ==================== Guardar usuario ====================
+    public static void saveUser(String nombre, String apellido, int identificacion, String clave, int edad,
+            boolean esAdministrador) {
+        String filePath = "USUARIO.txt";
         try {
-            FileWriter fileWriter = new FileWriter("USUARIO.txt");
-            fileWriter.write(nombre + "\n");
-            fileWriter.write(apellido + "\n");
-            fileWriter.write(clave + "\n");
-            fileWriter.write(identificacion + "\n");
-            fileWriter.write(edad + "\n");
-            fileWriter.write(esAdministrador + "\n");
+            // Abrir el archivo en modo de escritura
+            FileWriter fileWriter = new FileWriter(filePath, true);
+
+            // Leer todas las líneas existentes y almacenarlas
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+            String linea;
+            StringBuilder contenido = new StringBuilder();
+            while ((linea = bufferedReader.readLine()) != null) {
+                contenido.append(linea).append("\n");
+            }
+            bufferedReader.close();
+
+            contenido.append(nombre).append(",");
+            contenido.append(apellido).append(",");
+            contenido.append(identificacion).append(",");
+            contenido.append(clave).append(",");
+            contenido.append(edad).append(",");
+            contenido.append(esAdministrador).append("\n");
             fileWriter.close();
-            System.out.println("Los datos se han guardado correctamente en el archivo.");
+
+            fileWriter = new FileWriter(filePath);
+            // Escribir todas las líneas en el archivo
+            fileWriter.write(contenido.toString());
+
+            fileWriter.close();
+            System.out.println("Se ha agregado el contenido correctamente");
+
         } catch (IOException e) {
-            System.out.println("Ha ocurrido un error al escribir en el archivo.");
             e.printStackTrace();
         }
     }
 
-    public static void administradorMenu() {
+    public static void deleteUser() {
+        File archivoIn = new File("USUARIO.txt");
+        File archivoOut = new File("NuevoUSUARIO.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero de identidad");
+        int identificacion = scanner.nextInt();
+        scanner.nextLine();
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoIn));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(archivoOut))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] atributos = linea.split(","); // Separar atributos por coma
+
+                boolean eliminarLinea = false;
+                for (String atributo : atributos) {
+                    if (Integer.parseInt(atributo) == identificacion) {
+                        eliminarLinea = true;
+                        break;
+                    }
+                }
+
+                if (!eliminarLinea) {
+                    bw.write(linea); // Escribir la línea en el archivo nuevo
+                    bw.newLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        archivoIn.delete();
+        archivoOut.renameTo(archivoIn);
+        System.out.println("Se ha eliminado corectamente");
+    }
+
+    public static void modifyUser() {
+        File archivoIn = new File("USUARIO.txt");
+        File archivoOut = new File("USUARIO.txt");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero de identidad");
+        int identificacion = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Que desea modificar");
+        System.out.println("1.Nombre");
+        System.out.println("2.Apellido");
+        System.out.println("3.Numero de identificacion");
+        System.out.println("4.Clave");
+        System.out.println("5.Edad");
+        int posicion = scanner.nextInt() - 1;
+        scanner.nextLine();
+        System.out.println("Ingrese el cambio");
+        String cambio = scanner.nextLine();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoIn));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(archivoOut))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] atributos = linea.split(","); // Separar atributos por coma
+                if (Integer.parseInt(atributos[2]) == identificacion) {
+                    for (int i = 0; i <= 4; i++) {
+                        if (Integer.parseInt(atributos[i]) == posicion) {
+                            atributos[i] = cambio;
+                            linea = atributos[0] + "," + atributos[1] + "," + atributos[2] + "," + atributos[3] + ","
+                                    + atributos[4];
+                            break;
+                        }
+                    }
+                }
+                bw.write(linea); // Escribir la línea en el archivo nuevo
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        archivoIn.delete();
+        archivoOut.renameTo(archivoIn);
+        System.out.println("Se ha eliminado corectamente");
+    }
+
+    public static void mostrarMenuAdmin() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("=== Administrador ===");
         System.out.println("=== Desea ingresar a: ===");
         System.out.println("1. Usuarios");
         System.out.println("2. Cuentas");
-        System.out.print("Selecciona una opción: ");
+        System.out.println("Selecciona una opcion: ");
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+        switch (opcion) {
+            case 1:
+                mostrarMenuUsuarios();
+                break;
+            default:
+                break;
+        }
+
     }
-    
-    public static void cuentasMenu() {
-        System.out.println("=== MENÚ ===");
-        System.out.println("1. Desea crear");
-        System.out.println("2. Desea borrar");
-        System.out.println("3. Desea modificar");
+
+    public static void mostrarMenuUsuarios() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=== Desea ===");
+        System.out.println("1. Crear");
+        System.out.println("2. Borrar");
+        System.out.println("3. Modificar");
+        System.out.print("Selecciona una opción: ");
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
+        switch (opcion) {
+            case 1:
+                createUser();
+                break;
+            case 2:
+                deleteUser();
+            default:
+                modifyUser();
+                break;
+        }
+
+    }
+
+    public static void mostrarMenuCuentas() {
+        System.out.println("=== Desea ===");
+        System.out.println("1. Crear");
+        System.out.println("2. Borrar");
+        System.out.println("3. Modificar");
         System.out.println("4. Ver reporte de cuentas");
         System.out.print("Selecciona una opción: ");
     }
-
 }
